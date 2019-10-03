38983a0690b85d729fdd55087c7e32f3737dbd24
jcorea <jacorea410@gmail.com>
2019-10-03 16:19:21 -0700

    reconfigured routes file and implemented logic for CRUD actions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'todos/index'
-  get 'todos/create'
-  get 'todos/update'
-  get 'todos/destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope '/api/v1' do 
+    resources :todos  
+  end
 end

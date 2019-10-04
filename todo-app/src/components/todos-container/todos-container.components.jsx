cae6e28038ff3f683831b7a37ea23d47203acddd
jcorea <jacorea410@gmail.com>
2019-10-03 19:45:51 -0700

    added immutability helper to update todos array. Additionally, added createTodo to todo list.

--- todo-app/src/components/todos-container/todos-container.components.jsx
+++ todo-app/src/components/todos-container/todos-container.components.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import update from 'immutability-helper';
 
 //scss files
 import './todos-container.styles.scss'
@@ -8,7 +9,8 @@ class TodosContainer extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            todos: []
+            todos: [],
+            inputValue: ''
         }
     }
 
@@ -27,10 +29,14 @@ class TodosContainer extends React.Component {
                 const todos = update(this.state.todos, {
                     $splice: [[0,0,response.data]]
                 })
-                this.setState({todos: todos})
+                this.setState({todos: todos, inputValue: ''})
             })
             .catch(error => console.log(error))
         }
+    }
+
+    handleChange = (e) => {
+        this.setState({inputValue: e.target.value })
     }
 
     componentDidMount() {
@@ -41,11 +47,11 @@ class TodosContainer extends React.Component {
         return (
             <div>
                 <div className="input-container">
-                    <input 
-                        className="task-input" 
-                        type="text" placeholder="Add a task"
+                    <input className="task-input" type="text" placeholder="Add a task"
                         maxLength="50" 
-                        onKeyPress={this.createTodo} 
+                        onKeyPress={this.createTodo}
+                        onChange={this.handleChange}
+                        value={this.state.inputValue} 
                     />
                 </div>
                 <div className="listWrapper">
@@ -54,7 +60,7 @@ class TodosContainer extends React.Component {
                             return (
                                 <li className="task" todo={todo} key={todo.id} >
                                     <input type="checkbox" className="taskCheckbox"/>
-                                    <label className="taskLabel">{todo.id}</label>
+                                    <label className="taskLabel">{todo.title}</label>
                                     <span className="deleteTaskBtn">X</span>
                                 </li>
                             )
